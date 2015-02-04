4a808f8d93ec71e3ab3cdfaf070cd39d5eee5195
Rick Windham <rick.windham@gmail.com>
2015-02-04 13:24:25 -0500

    Initial Commit

---
+++ SwiftlyReachable/SwiftlyReachable.h
@@ -0,0 +1,19 @@
+//
+//  SwiftlyReachable.h
+//  SwiftlyReachable
+//
+//  Created by Rick Windham on 2/4/15.
+//  Copyright (c) 2015 SwiftTime. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftlyReachable.
+FOUNDATION_EXPORT double SwiftlyReachableVersionNumber;
+
+//! Project version string for SwiftlyReachable.
+FOUNDATION_EXPORT const unsigned char SwiftlyReachableVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftlyReachable/PublicHeader.h>
+
+
