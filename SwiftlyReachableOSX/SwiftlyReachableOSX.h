e783fca01d5ba850dab8681559ffc95efc1aa089
Rick Windham <rick.windham@gmail.com>
2015-02-11 16:39:57 -0500

    added OSX target

---
+++ SwiftlyReachableOSX/SwiftlyReachableOSX.h
@@ -0,0 +1,19 @@
+//
+//  SwiftlyReachableOSX.h
+//  SwiftlyReachableOSX
+//
+//  Created by Rick Windham on 2/11/15.
+//  Copyright (c) 2015 SwiftTime. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for SwiftlyReachableOSX.
+FOUNDATION_EXPORT double SwiftlyReachableOSXVersionNumber;
+
+//! Project version string for SwiftlyReachableOSX.
+FOUNDATION_EXPORT const unsigned char SwiftlyReachableOSXVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftlyReachableOSX/PublicHeader.h>
+
+
